82e2ffb62b63355638bfb6f8b681b1f2ec8dff7b
Leon Brett <leon_brett@live.co.uk>
2018-09-27 21:23:02 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Simple debounce
+A simple utility just to debounce events
---
+++ src/debounce.js
@@ -0,0 +1,21 @@
+/**
+ * Simple Debounce
+ *
+ * @author Leon Brett
+ * @export simple debounce
+ * @param {function} func the function to debounce
+ * @param {number} delay the delay in milliseconds
+ * @returns a new debounce method(call this to debounce)
+ */
+export default function (func, delay) {
+  let timeout = null;
+  return (...args) => {
+    const context = this;
+    const bounce = () => {
+      timeout = null;
+      func.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(bounce, delay);
+  };
+}
