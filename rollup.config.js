d6bb65444141d46845da433c774de8416b572788
Leon Brett <leon@accentdesign.co.uk>
2019-02-05 16:13:59 +0000

    updated packages and added babel transforms

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/debounce.js',
+  output: {
+    file: 'dist/debounce.js',
+    format: 'cjs'
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ["@babel/plugin-transform-parameters"]
+    })
+  ]
+};
